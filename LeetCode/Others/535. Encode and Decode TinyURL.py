455699422484cba87e80118af2b8d87dc37c2e41
ZhangJiaQ <529326573@qq.com>
2018-01-05 09:57:56 +0800

    zjq

--- LeetCode/Others/535. Encode and Decode TinyURL.py
+++ LeetCode/Others/535. Encode and Decode TinyURL.py
@@ -4,7 +4,6 @@
 
 #Design the encode and decode methods for the TinyURL service. There is no restriction on how your encode/decode algorithm should work. You just need to ensure that a URL can be encoded to a tiny URL and the tiny URL can be decoded to the original URL.
 
-
 import random
 class Codec:
     
