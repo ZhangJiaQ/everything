43633b8c735c418b86d4eb6302167690813f43e1
ZhangJiaQ <529326573@qq.com>
2018-01-10 15:52:27 +0800

    1/10

--- LintCode/数据结构/Lintcode40.py
+++ LintCode/数据结构/Lintcode40.py
@@ -6,6 +6,8 @@ As the title described, you should only use two stacks to implement a queue's ac
 The queue should support push(element), pop() and top() where pop is pop the first(a.k.a front) element in the queue.
 
 Both pop and top methods should return the value of first element.
+
+And I don't know why i can't push it on github
 '''
 
 class MyQueue:
