69e5b99e3601ef0122cbaf6012d8b0ec05871816
Judge_Zhang <judgezhang123@163.com>
2021-01-07 09:15:37 +0800

    backtrack

--- algorithm/backtrack/89. Gray Code.py
+++ algorithm/backtrack/89. Gray Code.py
@@ -12,4 +12,33 @@ class Solution:
     """
 
     def grayCode(self, n: int) -> List[int]:
-        pass
+        seen = {0}
+
+        def back_track(results):
+            if len(results) == 2 ** n:
+                print(1111111)
+                return results
+            for i in range(n):
+                now = 1 << i ^ results[-1]
+                if now in seen:
+                    continue
+                print(results, now)
+                results.append(now)
+                print(results, now)
+                seen.add(now)
+                if back_track(results):
+                    return results
+                results.pop()
+                seen.remove(now)
+
+        return back_track([0])
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.grayCode(1))
+    # print(s.grayCode(2))
+    print(s.grayCode(3))
+    # print(s.grayCode(4))
+    # print(s.grayCode(5))
+    # print(s.grayCode(6))
