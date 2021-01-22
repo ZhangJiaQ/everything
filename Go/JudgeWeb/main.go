6d5ec2c1b0b2109101f3f770c5cd2ddec3b1d566
Judge_Zhang <judgezhang123@163.com>
2021-01-22 22:16:33 +0800

    web

--- Go/JudgeWeb/main.go
+++ Go/JudgeWeb/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Print("Hi I am Judge")
+	http.HandleFunc("/", handler)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+
 }
