ed9d55e3c7afb2a502e5310491891a99dd475311
Judge_Zhang <judgezhang123@163.com>
2021-01-29 23:25:47 +0800

    stock

---
+++ algorithm/stock/121. Best Time to Buy and Sell Stock.py
@@ -0,0 +1,33 @@
+from typing import List
+
+
+class Solution:
+
+    def maxProfit(self, prices: List[int]) -> int:
+        """
+        @param prices:
+        @return:
+        要有状态转移方程的思路
+          首先需要定义出状态转移方程
+          dp[-1][k][0] = 0
+            解释：因为 i 是从 0 开始的，所以 i = -1 意味着还没有开始，这时候的利润当然是 0 。
+          dp[-1][k][1] = -infinity
+            解释：还没开始的时候，是不可能持有股票的，用负无穷表示这种不可能。
+          dp[i][0][0] = 0
+            解释：因为 k 是从 1 开始的，所以 k = 0 意味着根本不允许交易，这时候利润当然是 0 。
+          dp[i][0][1] = -infinity
+            解释：不允许交易的情况下，是不可能持有股票的，用负无穷表示这种不可能。
+        """
+        length = len(prices)
+        dp = [[[0, 0]] * length]
+
+        dp[-1][1][0], dp[length][0][0] = 0, 0
+        dp[-1][1][1], dp[length][0][1] = -float("INF")
+
+        for index, price in enumerate(prices):
+            dp[index][]
+
+
+if __name__ == '__main__':
+    s = Solution()
+    s.maxProfit([1,2,3,4,5])
