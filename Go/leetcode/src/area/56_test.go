214e1a1148fcb431a4673b85de7e896fa64f899f
ZhangJiaQ <529326573@qq.com>
2021-02-12 23:56:53 +0800

    'area'

---
+++ Go/leetcode/src/area/56_test.go
@@ -0,0 +1,15 @@
+package Area
+
+import (
+	"fmt"
+	"testing"
+)
+
+func Test56(t *testing.T) {
+
+	nums := [][]int{{1,5},{1,4},{3,6},{2,8}}
+	result := merge(nums)
+	fmt.Println(``)
+	fmt.Println(result)
+	fmt.Println(``)
+}
