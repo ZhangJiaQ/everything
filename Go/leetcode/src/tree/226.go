469365ab4a869fa4d474692f3b9ea1e8f00496b9
Judge_Zhang <judgezhang123@163.com>
2021-03-09 22:03:01 +0800

    tree

---
+++ Go/leetcode/src/tree/226.go
@@ -0,0 +1,27 @@
+package tree
+
+
+
+type TreeNode struct {
+     Val int
+     Left *TreeNode
+     Right *TreeNode
+}
+
+func invertTree(root *TreeNode) *TreeNode {
+
+	if root == nil {
+		return root
+	}
+
+	root.Left, root.Right = root.Right, root.Left
+
+	if root.Left != nil {
+		invertTree(root.Left)
+	}
+	if root.Right != nil {
+		invertTree(root.Right)
+	}
+
+	return root
+}
