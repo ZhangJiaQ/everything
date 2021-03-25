d8f3c71001dd560c391641201b8cf145cf923bf2
Judge_Zhang <judgezhang123@163.com>
2021-03-25 23:29:57 +0800

    20210325

---
+++ Go/leetcode/src/linknode/82.go
@@ -0,0 +1,40 @@
+package linknode
+
+type ListNode struct {
+
+		Val int
+		Next *ListNode
+
+}
+
+
+
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+
+
+	firstHead := &ListNode{Val: 0, Next: head}
+
+	first := firstHead
+	next := firstHead.Next
+
+	for next != nil {
+		if next.Next != nil && next.Next.Val == next.Val {
+			delVal := next.Val
+			for next != nil && next.Val == delVal {
+				// 移除所有有重复的数字
+				first.Next = next.Next
+				next.Next = nil
+				next = first.Next
+			}
+		} else {
+			first = first.Next
+			next = next.Next
+		}
+
+	}
+	return firstHead.Next
+
+}
